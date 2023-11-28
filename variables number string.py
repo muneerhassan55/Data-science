e5e5aad4f9db10fb16de33a5f7e4dd062007682c
Muneer Hasan <102030892+muneerhassan55@users.noreply.github.com>
2023-11-28 12:14:26 +0500

    Add files via upload
    This file contains variables number and string in python

---
+++ variables number string.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 11:56:22 2023
+
+@author: Muneer Hasan
+"""
+#Variables in python
+rent=1200
+gas_bill=240
+groceries=900
+total=rent+gas_bill+groceries
+print(rent)
+print(gas_bill)
+print(groceries)
+print("Total expensis are " +str(total))
+
+#Number in python
+print(2+6)
+print(2-6)
+print(2*6)
+print(6/2)
+print(11%2)
+print(3**2)
+
+#Strings in pyhton'
+f_name="Muneer"
+l_name="Hasan"
+print(f_name,l_name)
